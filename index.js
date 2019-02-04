1cfd0e4da23462a13737056d3c8baec84a89de2a
Antonio Bispo <antoniobispo@softeam.com.br>
2019-02-04 19:15:43 -0300

    adicionando documentacao e configurando o travis

--- index.js
+++ index.js
@@ -3,14 +3,19 @@ const bodyParser = require('body-parser');
 const pg = require('pg');
 const researcherRouter = require('./src/researcher/researcherRouter');
 const studyRouter = require('./src/study/studyRouter');
+const swaggerUi = require('swagger-ui-express');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', (req, res) => res.send('Hello Word'));
 app.use('/researcher', researcherRouter());
 app.use('/study', studyRouter());
 
-module.exports = app.listen(5000, console.log("ta rodando"));
+const swaggerDocument = require('./docs/documentation.json');
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.listen(5000, console.log("ta rodando"));
+
+module.exports = app
